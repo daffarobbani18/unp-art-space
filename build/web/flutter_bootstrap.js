a9a5ca8e8027dcddcde786f44f0f2ca2309c83a4
Daffa Robbani <d.robbani18@gmail.com>
2025-11-27 23:32:25 +0700

    fix: correct admin dashboard queries to use profiles table and approval_status column

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3097832470"
+    serviceWorkerVersion: "4022964839"
   }
 });
