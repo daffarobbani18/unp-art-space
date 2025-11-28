7ae7a2ad5b83d3fdbd06984de44b215d4bac003d
Daffa Robbani <d.robbani18@gmail.com>
2025-11-29 03:24:16 +0700

    build(web): update web-deploy with QR code URL fix
    Rebuild web version with corrected QR code URL format.
    QR codes now point to /submission/{uuid} instead of /artwork/{id}.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1980020885"
+    serviceWorkerVersion: "3535956969"
   }
 });
